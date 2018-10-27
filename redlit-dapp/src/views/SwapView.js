16e6728daf395d03c3bdfcd726f6a959273e466d
fran <fberdun@gmail.com>
2018-10-27 16:53:38 +0200

    refresh

--- redlit-dapp/src/views/SwapView.js
+++ redlit-dapp/src/views/SwapView.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 const SwapView  = (props) => (
       <div className="subview">
@@ -8,8 +10,18 @@ const SwapView  = (props) => (
           <p className="swap-text">
             Your Swap!!
           </p>
-
         </div>
+
+        <div>
+          <img src={require('../img/user.svg')} height="158px" width="480px"/>
+        </div>
+        <div>
+          <img src={require('../img/CrossMark.png')} height="150px" width="150px" circle />
+          <img src={require('../img/CheckMark.png')} height="150px" width="150px" circle />
+        </div>
+      <div>
+          <Slider />
+      </div>
 
       <div className="text-center">
         <button onClick={() => props.setView('Account')} className="secondary-btn">
